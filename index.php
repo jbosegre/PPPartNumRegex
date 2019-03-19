4d50331c200ed222d0800d176f31c1181431c9cd
ttnny <ttnny@users.noreply.github.com>
2019-03-19 02:15:44 -0400

    Update Regex

--- index.php
+++ index.php
@@ -1,41 +1,61 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: humme_000
- * Date: 2/20/2019
- * Time: 11:22 AM
- */
-
-function validPart($str)
-{
-    $input = trim($str);
-    $input = explode("-", $input);
-    $category = array("HW", "SG", "AP", "hw", "sg", "ap");
-    $houseCategory = substr($str, 0,2);
-    if(!in_array($houseCategory, $category)){
-        echo "False";
-    }
-}
 
 $parts = array("AP-12-3507", "ap-99-X109", "SG-05-ab20", "ab-22-N250",
     "SG-xx-N250", "SG-22-250", "SG-22-250*");
-foreach($parts as $item)
+
+// Validation without Regex
+function validPart($str)
 {
+    $isValid = true;
+
+    $input = trim($str);
+    $input = str_replace("-", "", $input);
+
+    $category = array("HW", "SG", "AP", "hw", "sg", "ap");
+    $inputCategory = substr($input, 0, 2);
+    if (!in_array($inputCategory, $category)) {
+        $isValid = false;
+    }
+
+    $inputWarehouse = substr($input, 2, 2);
+    if (!is_numeric($inputWarehouse)) {
+        $isValid = false;
+    }
+
+    $inputPart = str_split(substr($input, 4, 4));
+    if (count($inputPart) != 4) {
+        $isValid = false;
+    }
+
+    $specialCharacters = str_split("~!@#$%^&*()-=+[]{};':,.<>/?\|");
+    foreach ($inputPart as $char) {
+        if (in_array($char, $specialCharacters)) {
+            $isValid = false;
+        }
+    }
+
+    return $isValid;
+}
+
+echo "<p>WITHOUT Regex</p>";
+foreach ($parts as $item) {
     if (validPart($item))
         echo "$item is valid<br />";
     else
         echo "$item is not valid<br />";
 }
 
-/*
+// Validation with Regex
 function validPartRx($str)
 {
     $regexp = '/^[(HW)(SG)(AP)]+-\d{2}-\w{4}$/im';
     return preg_match($regexp, $str);
 }
 
-foreach($parts as $item)
-{
+echo "<br>";
+
+echo "<p>WITH Regex</p>";
+foreach ($parts as $item) {
     if (validPartRx($item))
         echo "$item is valid<br />";
     else
